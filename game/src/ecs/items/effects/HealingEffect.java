d73b0532d04d982df6e10d5dfbcfc958c172ae92
fni@remmert.de <Yu_@gQ=(A.bq%6NL>
2023-04-25 11:46:18 +0200

    implemented Healing, Speed, and Damage effects in their respective classes

---
+++ game/src/ecs/items/effects/HealingEffect.java
@@ -0,0 +1,47 @@
+package ecs.items.effects;
+
+import ecs.components.HealthComponent;
+import ecs.components.InventoryComponent;
+import ecs.entities.Entity;
+import ecs.items.IOnUse;
+import ecs.items.ItemData;
+
+import java.util.Random;
+
+public class HealingEffect implements IOnUse {
+
+    private int healthPoints;
+    private Random random;
+
+    @Override
+    public void onUse(Entity e, ItemData item) {
+        e.getComponent(HealthComponent.class)
+            .ifPresent(hc -> {
+                int currentHP = ((HealthComponent) hc).getCurrentHealthpoints();
+                int maxHP = ((HealthComponent) hc).getMaximalHealthpoints();
+
+                if (currentHP < maxHP) {
+                    int min = 1;
+
+                    healthPoints = random.nextInt(maxHP - min + 1) + min;
+                    int finalHP = currentHP + healthPoints;
+
+                    if (finalHP <= maxHP) {
+                        ((HealthComponent) hc).setCurrentHealthpoints(finalHP);
+
+                        e.getComponent(InventoryComponent.class)
+                            .ifPresent(ic -> {
+                                InventoryComponent inventoryComponent = (InventoryComponent) ic;
+                                ((InventoryComponent) ic).removeItem(item);
+                            });
+
+                        System.out.printf("%s hat %d HP regeneriert!\n", e.id, finalHP);;
+                    } else {
+                        System.out.println("Trank konnte nicht verwendet werden.");
+                    }
+                } else {
+                    System.out.printf("%s hast schon die maximale Anzahl an HP.\n", e.id);
+                }
+            });
+    }
+}
---
+++ game/src/ecs/items/effects/SpeedEffect.java
@@ -0,0 +1,33 @@
+package ecs.items.effects;
+
+import ecs.components.VelocityComponent;
+import ecs.entities.Entity;
+import ecs.items.IOnUse;
+import ecs.items.ItemData;
+
+import java.util.Random;
+
+public class SpeedEffect implements IOnUse {
+
+    private float speedPoints;
+    private Random random;
+
+    @Override
+    public void onUse(Entity e, ItemData item) {
+        e.getComponent(VelocityComponent.class)
+            .ifPresent(vc -> {
+                float maxXSpeed = ((VelocityComponent) vc).getXVelocity();
+                float maxYSpeed = ((VelocityComponent) vc).getYVelocity();
+
+                float min = maxXSpeed / 4;
+                float max = maxXSpeed / 2;
+
+                float velocityBoost = random.nextFloat(max) + min;
+
+                ((VelocityComponent) vc).setCurrentXVelocity(maxXSpeed + velocityBoost);
+                ((VelocityComponent) vc).setCurrentYVelocity(maxYSpeed + velocityBoost);
+
+                System.out.printf("Deine Geschwindigkeit wurde um %f erhöht", velocityBoost);
+            });
+    }
+}
