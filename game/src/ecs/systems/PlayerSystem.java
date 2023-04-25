7605f39f5733c3d5c627933e2811f8abd958c871
tschnittger <64741047+tschnittger@users.noreply.github.com>
2023-04-25 15:56:45 +0200

    Added option to open inventory in checkKeystroke

--- game/src/ecs/systems/PlayerSystem.java
+++ game/src/ecs/systems/PlayerSystem.java
@@ -13,7 +13,7 @@ import starter.Game;
 public class PlayerSystem extends ECS_System {
 
     private record KSData(Entity e, PlayableComponent pc, VelocityComponent vc) {}
-
+    private boolean inventoryOpen=false;
     @Override
     public void update() {
         Game.getEntities().stream()
@@ -23,25 +23,43 @@ public class PlayerSystem extends ECS_System {
     }
 
     private void checkKeystroke(KSData ksd) {
-        if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_UP.get()))
-            ksd.vc.setCurrentYVelocity(1 * ksd.vc.getYVelocity());
-        else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_DOWN.get()))
-            ksd.vc.setCurrentYVelocity(-1 * ksd.vc.getYVelocity());
-        else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_RIGHT.get()))
-            ksd.vc.setCurrentXVelocity(1 * ksd.vc.getXVelocity());
-        else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_LEFT.get()))
-            ksd.vc.setCurrentXVelocity(-1 * ksd.vc.getXVelocity());
+        if (!inventoryOpen) {
+            if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_UP.get()))
+                ksd.vc.setCurrentYVelocity(1 * ksd.vc.getYVelocity());
+            else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_DOWN.get()))
+                ksd.vc.setCurrentYVelocity(-1 * ksd.vc.getYVelocity());
+            else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_RIGHT.get()))
+                ksd.vc.setCurrentXVelocity(1 * ksd.vc.getXVelocity());
+            else if (Gdx.input.isKeyPressed(KeyboardConfig.MOVEMENT_LEFT.get()))
+                ksd.vc.setCurrentXVelocity(-1 * ksd.vc.getXVelocity());
 
-        if (Gdx.input.isKeyPressed(KeyboardConfig.INTERACT_WORLD.get()))
-            InteractionTool.interactWithClosestInteractable(ksd.e);
+            if (Gdx.input.isKeyPressed(KeyboardConfig.INTERACT_WORLD.get()))
+                InteractionTool.interactWithClosestInteractable(ksd.e);
+            else if (Gdx.input.isKeyJustPressed(KeyboardConfig.INVENTORY_OPEN.get())) {
+                inventoryOpen=true;
+                System.out.println("Inventory opened!\n");
 
-        // check skills
-        else if (Gdx.input.isKeyPressed(KeyboardConfig.FIRST_SKILL.get()))
-            ksd.pc.getSkillSlot1().ifPresent(skill -> skill.execute(ksd.e));
-        else if (Gdx.input.isKeyPressed(KeyboardConfig.SECOND_SKILL.get()))
-            ksd.pc.getSkillSlot2().ifPresent(skill -> skill.execute(ksd.e));
+            }
+            // check skills
+            else if (Gdx.input.isKeyPressed(KeyboardConfig.FIRST_SKILL.get()))
+                ksd.pc.getSkillSlot1().ifPresent(skill -> skill.execute(ksd.e));
+            else if (Gdx.input.isKeyPressed(KeyboardConfig.SECOND_SKILL.get()))
+                ksd.pc.getSkillSlot2().ifPresent(skill -> skill.execute(ksd.e));
+        } else {
+            if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_UP.get())) {
+                //inv up
+            }
+            else if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_DOWN.get())) {
+                //inv down
+            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.INTERACT_WORLD.get())) {
+                //activate item
+            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.INVENTORY_OPEN.get())) {
+                inventoryOpen=false;
+                System.out.println("Inventory closed!\n");
+            }
+
+        }
     }
-
     private KSData buildDataObject(PlayableComponent pc) {
         Entity e = pc.getEntity();
 
