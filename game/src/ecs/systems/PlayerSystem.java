e21482b2a29c7b6d2b8595b02b6785b16fef8801
tschnittger <64741047+tschnittger@users.noreply.github.com>
2023-04-26 21:28:17 +0200

    Fixed inventory and bags in Playersystem

--- game/src/ecs/systems/PlayerSystem.java
+++ game/src/ecs/systems/PlayerSystem.java
@@ -6,6 +6,7 @@ import ecs.components.*;
 import ecs.entities.Bag;
 import ecs.entities.Entity;
 import ecs.entities.Hero;
+import ecs.items.ItemCategory;
 import ecs.items.ItemData;
 import ecs.items.ItemType;
 import ecs.items.WorldItemBuilder;
@@ -27,8 +28,8 @@ public class PlayerSystem extends ECS_System {
 
     private boolean inventoryOpen = false;
     private boolean bagOpen = false;
-    private int inventoryItemSlotCounter = -1;
-    private int bagItemSlotCounter = -1;
+    private int inventoryItemSlotCounter = 0;
+    private int bagItemSlotCounter = 0;
 
     @Override
     public void update() {
@@ -66,10 +67,10 @@ public class PlayerSystem extends ECS_System {
                 inventoryOpen = false;
                 System.out.println("Inventory closed!\n");
             }
-            if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_UP.get())) {
+            if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_RIGHT.get())) {
                 inventoryItemSlotCounter++;
                 accessInventoryPrintItem(ksd.e, inventoryItemSlotCounter);
-            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_DOWN.get())) {
+            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_LEFT.get())) {
                 inventoryItemSlotCounter--;
                 accessInventoryPrintItem(ksd.e, inventoryItemSlotCounter);
             }
@@ -78,25 +79,30 @@ public class PlayerSystem extends ECS_System {
             }
             if (Gdx.input.isKeyJustPressed(KeyboardConfig.INVENTORY_THROW.get())) {
                 accessInventoryDropItem(ksd.e, inventoryItemSlotCounter);
+                inventoryItemSlotCounter=0;
             }
 
-        } else if (bagOpen) {
+        } else {
             if (Gdx.input.isKeyJustPressed(KeyboardConfig.INVENTORY_OPEN.get())) {
                 bagOpen = false;
-                System.out.println("Bag closed!\n");
+                System.out.println("Bag closed!\nBack to regular Inventory");
+                accessInventoryPrintInventory(ksd.e);
+                inventoryItemSlotCounter = 0;
             }
-            if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_UP.get())) {
+            if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_RIGHT.get())) {
                 bagItemSlotCounter++;
-                accessInventoryPrintItem(ksd.e, bagItemSlotCounter);
-            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_DOWN.get())) {
+                accessBagPrintItem(ksd.e, inventoryItemSlotCounter, bagItemSlotCounter);
+            } else if (Gdx.input.isKeyJustPressed(KeyboardConfig.MOVEMENT_LEFT.get())) {
                 bagItemSlotCounter--;
-                accessInventoryPrintItem(ksd.e, bagItemSlotCounter);
+                accessBagPrintItem(ksd.e, inventoryItemSlotCounter, bagItemSlotCounter);
             }
             if (Gdx.input.isKeyJustPressed(KeyboardConfig.INTERACT_WORLD.get())) {
-                //activate item
+                accessBagUseActiveItem(ksd.e, inventoryItemSlotCounter, bagItemSlotCounter);
             }
             if (Gdx.input.isKeyJustPressed(KeyboardConfig.INVENTORY_THROW.get())) {
-                accessInventoryDropItem(ksd.e, bagItemSlotCounter);
+                accessBagDropItem(ksd.e, inventoryItemSlotCounter, bagItemSlotCounter);
+                bagItemSlotCounter=0;
+                accessBagPrintItem(ksd.e, inventoryItemSlotCounter, bagItemSlotCounter);
             }
         }
     }
@@ -112,6 +118,7 @@ public class PlayerSystem extends ECS_System {
         return new KSData(e, pc, vc);
     }
 
+    //Todo write exception for drop
     private static MissingComponentException missingVC() {
         return new MissingComponentException("VelocityComponent");
     }
@@ -140,8 +147,6 @@ public class PlayerSystem extends ECS_System {
                         ((InventoryComponent) ic).printInventory(inventoryItemSlotCounter);
                     }
                 });
-        } else {
-            //TODO add bag functionality
         }
     }
 
@@ -149,14 +154,28 @@ public class PlayerSystem extends ECS_System {
         e.getComponent(InventoryComponent.class)
             .ifPresent(ic -> {
                 if (((InventoryComponent) ic).getItems().get(slot).getItemType() == ItemType.Active) {
+                    System.out.println("Hey");
                     ((InventoryComponent) ic).getItems().get(slot).triggerUse(e);
-                }/* else if (((InventoryComponent) ic).getItems().get(slot).getItemType() == ItemType.BAG) {
-                    bagOpen=true;
-                }*/
+                } else if (((InventoryComponent) ic).getItems().get(slot).getItemCategory() == ItemCategory.BAG) {
+                    System.out.println("he");
+                    int bagSlot = ((InventoryComponent) ic).getItems().get(slot).getInventorySlot();
+                    if (((InventoryComponent) ic).getBagItems(bagSlot).size() > 0) {
+                        bagOpen = true;
+                        accessBagPrintBag(e, slot);
+                    } else {
+                        System.out.println("This bitch empty\n");
+                    }
+                }
             });
     }
 
+    /**
+     *
+     * @param entity
+     * @param slot
+     */
     private void accessInventoryDropItem(Entity entity, int slot) {
+        //Todo check if item exists before dropping
         entity.getComponent(InventoryComponent.class)
             .ifPresent(ic -> {
                 if (slot >= 0 && slot < ((InventoryComponent) ic).filledSlots()) {
@@ -177,19 +196,79 @@ public class PlayerSystem extends ECS_System {
                     List<ItemData> itemData = inventoryComponent.getItems();
                     double count = itemData.size();
 
-                    IntStream.range(0, itemData.size())
-                        .forEach(
-                            index ->
-                                itemData.get(index)
-                                    .triggerDrop(
-                                        entity,
-                                        calculateDropPosition(
-                                            positionComponent, index / count)));
+                    itemData.get(slot)
+                        .triggerDrop(
+                            entity,
+                            calculateDropPosition(
+                                positionComponent, slot / count));
                     System.out.println("Item at " + slot + " removed");
                 }
             });
     }
 
+    private void accessBagPrintBag(Entity e, int bagSlot) {
+        e.getComponent(InventoryComponent.class).ifPresent(ic -> {
+            ((InventoryComponent) ic).printBagInventory(bagSlot, -1);
+        });
+    }
 
+    private void accessBagPrintItem(Entity e, int bagSlot, int itemSlot) {
+        e.getComponent(InventoryComponent.class)
+            .ifPresent(ic -> {
+                if (itemSlot >= 0) {
+                    if (itemSlot < ((InventoryComponent) ic).filledBagSlots(bagSlot)) {
+                        ((InventoryComponent) ic).printBagInventory(bagSlot, itemSlot);
+                    } else {
+                        bagItemSlotCounter = 0;
+                        ((InventoryComponent) ic).printBagInventory(bagSlot, 0);
+                    }
+                } else {
+                    bagItemSlotCounter = ((InventoryComponent) ic).filledBagSlots(bagSlot) - 1;
+                    ((InventoryComponent) ic).printBagInventory(bagSlot, itemSlot);
+                }
+            });
+    }
+
+    private void accessBagUseActiveItem(Entity e, int bagSlot, int itemSlot) {
+        e.getComponent(InventoryComponent.class)
+            .ifPresent(ic -> {
+                if (((InventoryComponent) ic).getBagInventory().get(bagSlot).get(itemSlot).getItemType() == ItemType.Active) {
+                    ((InventoryComponent) ic).getBagInventory().get(bagSlot).get(itemSlot).triggerUse(e);
+                }
+            });
+    }
+
+    private void accessBagDropItem(Entity entity, int bagSlot, int itemSlot) {
+        entity.getComponent(InventoryComponent.class)
+            .ifPresent(ic -> {
+                if (bagSlot >= 0 && bagSlot < ((InventoryComponent) ic).filledSlots()) {
+                    ((InventoryComponent) ic).removeItemFromBag(
+                        ((InventoryComponent) ic).getItems().get(bagSlot),
+                        ((InventoryComponent) ic).getBagInventory().get(bagSlot).get(itemSlot));
+
+                    InventoryComponent inventoryComponent =
+                        entity.getComponent(InventoryComponent.class)
+                            .map(InventoryComponent.class::cast)
+                            .orElseThrow(
+                                () ->
+                                    missingVC());
+                    PositionComponent positionComponent =
+                        entity.getComponent(PositionComponent.class)
+                            .map(PositionComponent.class::cast)
+                            .orElseThrow(
+                                () ->
+                                    missingVC());
+                    List<ItemData> itemData = inventoryComponent.getBagItems(bagSlot);
+                    double count = itemData.size();
+
+                    itemData.get(itemSlot)
+                        .triggerDrop(
+                            entity,
+                            calculateDropPosition(
+                                positionComponent, itemSlot / count));
+                    System.out.println("Item at " + itemSlot + " removed");
+                }
+            });
+    }
 
 }
